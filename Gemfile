b6423e82ed6af14d1804f327099e675504bcc11c
zigyr <2721400746@qq.com>
2026-04-18 16:43:55 +0800

    优化了几篇文章；新添front matter自动更新时间的显示；新增添草稿的存储

--- Gemfile
+++ Gemfile
@@ -2,13 +2,13 @@
 
 source "https://rubygems.org"
 
-gem "jekyll-theme-chirpy", "~> 7.5"
+gem "jekyll-theme-chirpy", "~> 7.5"             # chirpy主题
 
-gem "html-proofer", "~> 5.0", group: :test
+gem "html-proofer", "~> 5.0", group: :test      # 测试用
 
-platforms :windows, :jruby do
-  gem "tzinfo", ">= 1", "< 3"
+group :jekyll_plugins do
+  gem "tzinfo", ">= 1", "< 3"                   # 时间格式化
   gem "tzinfo-data"
 end
 
-gem "wdm", "~> 0.2.0", :platforms => [:windows]
+gem "wdm", "~> 0.2.0", :platforms => [:windows] # Windows监听文件变化
--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -1,14 +1,16 @@
 #!/usr/bin/env ruby
-#
-# Check for changed posts
 
 Jekyll::Hooks.register :posts, :post_init do |post|
+  # 修复 Jekyll 4.x 语法
+  next if post.data['draft']
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  file_path = post.path
+  commit_count = `git rev-list --count HEAD "#{file_path}"`.chomp.to_i
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+  if commit_count > 0
+    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{file_path}"`.chomp
+    post.data['last_modified_at'] = Time.parse(lastmod_date)
+  else
+    post.data['last_modified_at'] = File.mtime(post.path)
   end
-
 end
